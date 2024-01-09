8deabf00dc12120d4783f2adc1082193e00378fa
Naveen <70085321+naveen701526@users.noreply.github.com>
2024-01-09 19:36:55 +0530

    geeksforgeeks problem of the day 9th Jan 2024

---
+++ SearchPatternKMP.java
@@ -0,0 +1,15 @@
+class SearchPatternKMP
+{
+    
+    ArrayList<Integer> search(String pat, String txt)
+    {
+        // your code here
+        ArrayList<Integer> list=new ArrayList<>();
+        int index=txt.indexOf(pat);
+        while(index!=-1){
+            list.add(index+1);
+            index=txt.indexOf(pat,index+1);
+        }
+        return list;
+    }
+}
