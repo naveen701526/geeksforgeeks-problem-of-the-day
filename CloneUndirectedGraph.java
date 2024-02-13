e00a9a80c04ac7c95908efb70886031831d6074a
Naveen <70085321+naveen701526@users.noreply.github.com>
2024-02-13 18:20:33 +0530

    geeks for geeks problem of the day CloneUndirectedGraph.java

---
+++ CloneUndirectedGraph.java
@@ -0,0 +1,26 @@
+class CloneUndirectedGraph{
+    Node cloneGraph(Node node){
+        if(node == null){
+            return null;
+        }
+        Map<Node, Node> map = new HashMap<>();
+        return help(node, map);
+    }
+    
+    Node help(Node org, Map<Node, Node> map){
+        if(org == null){
+            return null;
+        }
+        
+        if(map.containsKey(org)){
+            return map.get(org);
+        }
+        Node clone = new Node(org.val);
+        map.put(org, clone);
+        
+        for(Node neighbor : org.neighbors){
+            clone.neighbors.add(help(neighbor, map));
+        }
+        return clone;
+    }
+}
